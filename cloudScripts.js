3a4ade9a61cfcd5aed16527930ef1b9266948a07
Oleg Chubin <o_chubin@indatalabs.com>
2019-07-20 18:58:32 +0300

    test

--- cloudScripts.js
+++ cloudScripts.js
@@ -1,10 +1,17 @@
 // CloudScript (JavaScript)
 handlers.helloWorld = function (args, context) {
+    var currentState; // here we are calculating the current player's game state
+    var request = {
+      "StatisticName": "Turns",
+      "MaxResultsCount": 20
+    }
+    var leaderboardData = server.GetLeaderboardAroundUser(request);
+	
     var message = "Hello " + currentPlayerId + "!";
     log.info(message);
     var inputValue = null;
     if (args && args.hasOwnProperty("inputValue"))
         inputValue = args.inputValue;
     log.debug("helloWorld:", { input: inputValue });
-    return { messageValue: message };
+    return leaderboardData;
 }
